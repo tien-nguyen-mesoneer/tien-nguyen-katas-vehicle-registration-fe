8247e6ceb0180f33e3f0efc73c5ca7b26ef8438b
tien-nguyen-mesoneer <tien.nguyen@mesoneer.io>
2024-08-14 21:41:54 +0700

    add login form for login page

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,9 +1,8 @@
-import { Button, ButtonProps } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { useAuth } from "@/context/auth-context";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { UserRole } from "@/lib/types";
-import { Navigate, useNavigate } from "react-router-dom";
-import { MOCK_USERS } from "@/lib/data";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 
 function LoginPage() {
   const { user, login } = useAuth();
@@ -13,56 +12,32 @@ function LoginPage() {
     return <Navigate to="/" />;
   }
 
-  const handleLogin = (role: UserRole, redirect: string) => {
-    login(role);
-    navigate(redirect);
-  };
-
   return (
-    <div className="w-max h-max space-y-20">
+    <div className="h-screen w-screen flex flex-col justify-center gap-14 items-center">
       <div>
         <p className="text-4xl font-semibold">Welcome!</p>
         <p className="text-xl font-medium">
-          Let's register your first vehicle.
+          Log in as a Police officer to manage registrations
         </p>
       </div>
-      <div className="flex gap-10">
-        <Profile
-          img={MOCK_USERS.GENERAL.img}
-          name={MOCK_USERS.GENERAL.role}
-          onClick={() => handleLogin(MOCK_USERS.GENERAL.role, "/vehicles")}
-          variant="secondary"
-        />
-        <Profile
-          img={MOCK_USERS.POLICE.img}
-          name={MOCK_USERS.POLICE.role}
-          onClick={() => handleLogin(MOCK_USERS.POLICE.role, "/vehicles")}
-        />
+      <div className="flex flex-col gap-2.5">
+        <Input type="text" defaultValue={"admin@gmail.com"} />
+        <Input type="password" defaultValue={"01234"} />
+        <Button
+          onClick={() => {
+            login();
+            navigate("/");
+          }}
+        >
+          Log in
+        </Button>
       </div>
+      <Separator className="w-96" />
+      <Link to="/" className="text-sm italic underline">
+        I'm not a police officer, I want to register a vehicle
+      </Link>
     </div>
   );
 }
-
-interface ProfileProps extends ButtonProps {
-  img: string;
-  name: string;
-  onClick: VoidFunction;
-}
-
-const Profile = ({ img, name, onClick, ...props }: ProfileProps) => {
-  return (
-    <Button
-      className="h-full flex flex-col justify-center gap-3 shadow-xl p-4 rounded-lg"
-      onClick={onClick}
-      {...props}
-    >
-      <Avatar className="w-36 h-36">
-        <AvatarImage src={img} />
-        <AvatarFallback>{name}</AvatarFallback>
-      </Avatar>
-      <p className="font-extrabold">{name}</p>
-    </Button>
-  );
-};
 
 export default LoginPage;
